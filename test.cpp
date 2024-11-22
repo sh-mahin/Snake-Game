db7e653f64e58c6985f4bbb3f9b88c14a624fb4f
shahmathossainmahin <shahmatmahin@gmail.com>
2024-11-22 22:23:47 +0600

    first commit

---
+++ test.cpp
@@ -0,0 +1,48 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_main.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    // Create a window
+    SDL_Window* window = SDL_CreateWindow(
+        "Simple SDL2 Window",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (window == nullptr) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // Main loop flag
+    bool running = true;
+
+    // Event handler
+    SDL_Event event;
+
+    // Main loop
+    while (running) {
+        while (SDL_PollEvent(&event) != 0) {
+            if (event.type == SDL_QUIT) {
+                running = false;
+            }
+        }
+    }
+
+    // Destroy window
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
